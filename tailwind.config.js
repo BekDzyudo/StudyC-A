7476116e1fc18a9124f60c209279a8172a159421
BekDzyudo <jalilovshohbek9909@gmail.com>
2023-01-24 23:25:07 +0300

    tailwind practic

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+      // "2xl": "1500px",
+    },
+  },
+  plugins: [],
+};
